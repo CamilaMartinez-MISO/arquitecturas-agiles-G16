4773c585766a7f1234fd6f686f792395d660f45e
Santiago Alvarez Soto <alvarezsoto13@gmail.com>
2024-09-07 16:36:01 -0500

    Send healthcheck messages to RabbitMQ

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -3,6 +3,7 @@ import os
 import time
 from datetime import datetime
 
+import pika
 import requests
 
 SERVICE_URLS = os.getenv("SERVICE_URLS", []).split(",")
@@ -11,25 +12,41 @@ CHECK_INTERVAL = os.getenv("CHECK_INTERVAL", 10)  # Interval in seconds
 
 logging.basicConfig(filename="health_check.log", level=logging.INFO)
 
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+
+channel.queue_declare(queue="health_check_queue", durable=True)
+
 
 def send_alert(message):
     if ALERT_API_URL:
         requests.post(ALERT_API_URL, json={"text": message})
 
 
+def log_message(message, log_fn=logging.info, send_alert=False):
+    log_fn(message)
+    channel.basic_publish(exchange="", routing_key="health_check_queue", body=message)
+    if send_alert:
+        send_alert(message)
+
+
 def check_service_health(service_url):
     try:
         response = requests.get(service_url)
         if response.status_code == 200:
-            logging.info(f"{datetime.now()}: {service_url} is healthy")
+            log_message(f"{datetime.now()}: {service_url} is healthy")
         else:
-            logging.error(
-                f"{datetime.now()}: {service_url} unhealthy, status code: {response.status_code}"
+            log_message(
+                f"{datetime.now()}: {service_url} unhealthy, status code: {response.status_code}",
+                logging.error,
+                send_alert=True,
             )
-            send_alert(f"{service_url} unhealthy: {response.status_code}")
     except requests.exceptions.RequestException as e:
-        logging.error(f"{datetime.now()}: {service_url} unreachable, error: {e}")
-        send_alert(f"{service_url} unreachable: {e}")
+        log_message(
+            f"{datetime.now()}: {service_url} unreachable, error: {e}",
+            logging.error,
+            send_alert=True,
+        )
 
 
 if __name__ == "__main__":
@@ -38,3 +55,5 @@ if __name__ == "__main__":
             check_service_health(service_url.strip())
         check_service_health()
         time.sleep(CHECK_INTERVAL)
+
+connection.close()
